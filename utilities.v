d92cbbd885b0de62e3f2bac3d4cedd15ae67789d
Gustavo Nunes Martins <martins.yo@gmail.com>
2021-05-01 00:33:01 +0200

    fusesoc better config

--- utilities.v
+++ utilities.v
@@ -1,13 +1,17 @@
-`define assertCaseEqual(signal, value, message) \
-        if (signal !== value) begin \
-            $display("ASSERTION FAILED assertCaseEqual in %s: %0d: signal !== value. signal is: %d (decimal), %h (hexa)",`__FILE__, `__LINE__,signal,signal); \
-            $display("  Reason: %s ",message);\
-            $finish; \
-        end
+`ifndef assertCaseEqual
+    `define assertCaseEqual(signal, value, message) \
+            if (signal !== value) begin \
+                $display("ASSERTION FAILED assertCaseEqual in %s: %0d: signal !== value. signal is: %d (decimal), %h (hexa)",`__FILE__, `__LINE__,signal,signal); \
+                $display("  Reason: %s ",message);\
+                $finish; \
+            end
+`endif
 
-`define assertCaseNotEqual(signal, value, message) \
-        if (signal === value) begin \
-            $display("ASSERTION FAILED assertCaseNotEqual in %s: %0d: signal === value. signal is: %d (decimal), %h (hexa)",`__FILE__, `__LINE__,signal,signal); \
-            $display("  Reason: %s ",message);\
-            $finish; \
-        end
+`ifndef assertCaseNotEqual
+    `define assertCaseNotEqual(signal, value, message) \
+            if (signal === value) begin \
+                $display("ASSERTION FAILED assertCaseNotEqual in %s: %0d: signal === value. signal is: %d (decimal), %h (hexa)",`__FILE__, `__LINE__,signal,signal); \
+                $display("  Reason: %s ",message);\
+                $finish; \
+            end
+`endif
