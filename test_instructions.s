1b66572922d74a7a44a9c3b4559c97527637a71a
Gustavo Nunes Martins <martins.yo@gmail.com>
2021-05-03 01:12:32 +0200

    Better comment on assembly test mechanism

--- test_instructions.s
+++ test_instructions.s
@@ -1,7 +1,9 @@
-addi x31,x0,0
+//Test ADDI
+addi x31,x0,0 		//Expected value
 ADDI x21, x0, 123 
 ADDI x20, x0, 123
-SUB x31, x20, x21
+SUB x31, x20, x21 	//Control variable: X31 is zero if and only if circuit is correct
+//Finish test
 addi x0,x0,0
 addi x0,x0,0
 addi x0,x0,0
