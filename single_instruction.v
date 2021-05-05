d2c61650892defc938b5d1bbccab0f4558e056bc
Gustavo Nunes Martins <martins.yo@gmail.com>
2021-05-05 22:23:28 +0200

    Started moving some control values into a deidicated control unit

--- single_instruction.v
+++ single_instruction.v
@@ -4,19 +4,20 @@ module single_instruction (clk, instruction);
     input clk;
     input wire [31:0] instruction;
     
-    wire wr_enable;
+    // Control Unit
+    ControlUnit controlUnit(.instruction(instruction), .register_write_enable(registerWriteEnable), .aluOperationCode(alu_opcode));
+
+    // Register memory
+    wire registerWriteEnable;
     wire [4:0] rd_address_a,rd_address_b,wr_address;
     wire [31:0] wr_data,data_out_a,data_out_b;
-
-    assign wr_enable=1;
-    
     register_memory reg_mem (
         .clk (clk),
         .rd_address_a (rd_address_a), .rd_address_b (rd_address_b),
-        .wr_enable (wr_enable), .wr_address (wr_address), .wr_data (wr_data), .data_out_a (data_out_a), .data_out_b (data_out_b));
+        .wr_enable (registerWriteEnable), .wr_address (wr_address), .wr_data (wr_data), .data_out_a (data_out_a), .data_out_b (data_out_b));
 
     //ALU
-    alu ALU(.opcode (alu_opcode),.left (left),.right (right),.result (result));
+    alu ALU(.opcode (alu_opcode),.left (aluLeftInput),.right (aluRightInput),.result (aluResult));
 
     // All instructions:
     wire [6:0] opcode=instruction[6:0];
@@ -26,29 +27,40 @@ module single_instruction (clk, instruction);
     assign rd = instruction[11:7];
     assign rs1 = instruction[19:15];
     assign rs2 = instruction[24:20];
-    wire [2:0] funct3=instruction[14:12];
 
     // ALU -> General
     wire [2:0] alu_opcode;
-    wire [31:0]  left,right, result;
+    wire [31:0]  aluLeftInput,aluRightInput, aluResult;
 
     //ALU -> Type I Instructions
     wire [11:0] imm=instruction[31:20];
-    wire [31:0] right_i;
-    assign right_i= {20'b0,imm};
-    assign left = data_out_b; assign rd_address_b=rs1;
+    wire [31:0] aluRightSourceImmediate;
+    assign aluRightSourceImmediate= {20'b0,imm};
+    assign aluLeftInput = data_out_b; assign rd_address_b=rs1;
     assign wr_address = rd;
-    assign wr_data = result;
+    assign wr_data = aluResult;
 
-    //ALU Control
-    assign alu_opcode=(((opcode===7'b 0110011) & (instruction[31:25]=== 7'b 0100000))? 3'b 100: funct3);
+    
 
     //ALU -> Type R Instructions
-    wire [31:0] right_r;
-    assign right_r=data_out_a; assign rd_address_a=rs2;
+    wire [31:0] aluRightSourceRegister;
+    assign aluRightSourceRegister=data_out_a; assign rd_address_a=rs2;
 
     // Register control
-    assign right =   (opcode === 7'b 0010011)? right_i : 
-                    ((opcode === 7'b 0110011)? right_r: 32'h 0);
+    assign aluRightInput =   (opcode === 7'b 0010011)? aluRightSourceImmediate : 
+                    ((opcode === 7'b 0110011)? aluRightSourceRegister: 32'h 0);
   
+endmodule
+
+
+module ControlUnit(instruction, register_write_enable, aluOperationCode);
+    input [31:0] instruction;
+    output wire register_write_enable;
+    output wire [2:0] aluOperationCode;
+    wire [6:0] opcode=instruction[6:0];
+    wire [2:0] funct3=instruction[14:12];
+
+    assign register_write_enable=1'b 1;
+    //ALU Control
+    assign aluOperationCode=(((opcode===7'b 0110011) & (instruction[31:25]=== 7'b 0100000))? 3'b 100: funct3);
 endmodule
