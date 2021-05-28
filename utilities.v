c3f568d3a4aa30111d03f5e8d999745bbd5aa507
Gustavo Nunes Martins <martins.yo@gmail.com>
2021-05-28 22:09:25 +0200

    Better colours on assert fail

--- utilities.v
+++ utilities.v
@@ -1,8 +1,10 @@
 `ifndef assertCaseEqual
     `define assertCaseEqual(signal, value, message) \
             if (signal !== value) begin \
+                $display("%c[1;31m",27);\
                 $display("ASSERTION FAILED assertCaseEqual in %s: %0d: signal !== value. signal is: %d (decimal), %h (hexa)",`__FILE__, `__LINE__,signal,signal); \
                 $display("  Reason: %s ",message);\
+                $display("%c[0m",27);\
                 $finish; \
             end
 `endif
@@ -10,8 +12,10 @@
 `ifndef assertCaseNotEqual
     `define assertCaseNotEqual(signal, value, message) \
             if (signal === value) begin \
+                $display("%c[1;31m",27);\
                 $display("ASSERTION FAILED assertCaseNotEqual in %s: %0d: signal === value. signal is: %d (decimal), %h (hexa)",`__FILE__, `__LINE__,signal,signal); \
                 $display("  Reason: %s ",message);\
+                $display("%c[0m",27);\
                 $finish; \
             end
 `endif
