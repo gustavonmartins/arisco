e466322c6eff8a33a7a94421783d07e1606a9565
Gustavo Nunes Martins <martins.yo@gmail.com>
2021-11-09 17:15:12 +0100

    Tests: More harmonization among tests

--- tests.sh
+++ tests.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -euo pipefail
-clang --target=riscv32 -march=rv32i tests/test_instructions.s       -c -o arquivo.o             && llvm-objcopy -O binary arquivo.o             --only-section .text\* arquivo.bin              && hexdump -ve '1/4 "%08x\n"' arquivo.bin               >   arquivo.mem
-clang --target=riscv32 -march=rv32i tests/test_memory.s             -c -o arquivo_memory.o      && llvm-objcopy -O binary arquivo_memory.o      --only-section .text\* arquivo_memory.bin       && hexdump -ve '1/4 "%08x\n"' arquivo_memory.bin        >   arquivo_memory.mem
-clang --target=riscv32 -march=rv32i tests/test_memory_lbu.s         -c -o arquivo_memory_lbu.o  && llvm-objcopy -O binary arquivo_memory_lbu.o  --only-section .text\* arquivo_memory_lbu.bin   && hexdump -ve '1/4 "%08x\n"' arquivo_memory_lbu.bin    >   arquivo_memory_lbu.mem
 
-iverilog -Wall -o register_memory_out                       tests/register_memory_tb.v                  &&  vvp register_memory_out
-iverilog -Wall -o pc_out                                    tests/pc_tb.v                               && vvp pc_out
-iverilog -Wall -o alu_out                                   tests/alu_tb.v                              && vvp alu_out
-iverilog -Wall -o mem_out                                   tests/memory_tb.v                           && vvp mem_out
-iverilog -Wall -o single_instruction_out                    tests/single_instruction_tb.v               &&  vvp single_instruction_out
-iverilog -Wall -o instructions_i_out                        tests/instructions_i_tb.v                   &&  vvp instructions_i_out
-iverilog -Wall -o consecutive_instructions_out              tests/consecutive_instructions_tb.v         &&  vvp consecutive_instructions_out
-iverilog -Wall -o assembly_instructions_out                 tests/assembly_instructions_tb.v            &&  vvp assembly_instructions_out
-iverilog -Wall -o branch_instructions_out                   tests/branch_instructions_tb.v              &&  vvp branch_instructions_out
-iverilog -Wall -o assembly_instructions_memory_out          tests/assembly_instructions_memory_tb.v     && vvp assembly_instructions_memory_out
-iverilog -Wall -o assembly_instructions_memory_lbu_out      -DMEMFILEPATH=\"arquivo_memory_lbu.mem\"    -DVCDFILEPATH=\"assembly_instructions_memory_lbu.vcd\"      tests/assembly_instructions_memory_lbu_tb.v && vvp assembly_instructions_memory_lbu_out
+iverilog -Wall -o register_memory_out.o                       tests/register_memory_tb.v                  &&    vvp register_memory_out.o
+iverilog -Wall -o pc_out.o                                    tests/pc_tb.v                               &&    vvp pc_out.o
+iverilog -Wall -o alu_out.o                                   tests/alu_tb.v                              &&    vvp alu_out.o
+iverilog -Wall -o mem_out.o                                   tests/memory_tb.v                           &&    vvp mem_out.o
+iverilog -Wall -o single_instruction_out.o                    tests/single_instruction_tb.v               &&    vvp single_instruction_out.o
+iverilog -Wall -o instructions_i_out.o                        tests/instructions_i_tb.v                   &&    vvp instructions_i_out.o
+iverilog -Wall -o consecutive_instructions_out.o              tests/consecutive_instructions_tb.v         &&    vvp consecutive_instructions_out.o
+iverilog -Wall -o branch_instructions_out.o                   tests/branch_instructions_tb.v              &&    vvp branch_instructions_out.o
+
+clang --target=riscv32 -march=rv32i tests/test_instructions.s       -c -o test_instructions.o           && llvm-objcopy -O binary test_instructions.o   --only-section .text\* test_instructions.bin    && hexdump -ve '1/4 "%08x\n"' test_instructions.bin     >   test_instructions.mem
+clang --target=riscv32 -march=rv32i tests/test_memory.s             -c -o test_memory.o                 && llvm-objcopy -O binary test_memory.o         --only-section .text\* test_memory.bin          && hexdump -ve '1/4 "%08x\n"' test_memory.bin           >   test_memory.mem
+clang --target=riscv32 -march=rv32i tests/test_memory_lbu.s         -c -o test_memory_lbu.o             && llvm-objcopy -O binary test_memory_lbu.o     --only-section .text\* test_memory_lbu.bin      && hexdump -ve '1/4 "%08x\n"' test_memory_lbu.bin       >   test_memory_lbu.mem
+
+iverilog -Wall -o assembly_instructions_out.o                 tests/assembly_instructions_tb.v -DMEMFILEPATH=\"test_instructions.mem\"     -DVCDFILEPATH=\"test_instructions.vcd\"     && vvp assembly_instructions_out.o
+iverilog -Wall -o assembly_instructions_memory_out.o          tests/assembly_instructions_tb.v -DMEMFILEPATH=\"test_memory.mem\"           -DVCDFILEPATH=\"test_memory.vcd\"           && vvp assembly_instructions_memory_out.o
+iverilog -Wall -o assembly_instructions_memory_lbu_out.o      tests/assembly_instructions_tb.v -DMEMFILEPATH=\"test_memory_lbu.mem\"       -DVCDFILEPATH=\"test_memory_lbu.vcd\"       && vvp assembly_instructions_memory_lbu_out.o
--- tests/assembly_instructions_tb.v
+++ tests/assembly_instructions_tb.v
@@ -2,7 +2,11 @@
 `include "rtl/utilities.v"
 `include "rtl/multiple_instructions.v"
 
-module assembly_instructions_tb ();
+module assembly_instructions_memory_lbu_tb ();
+//Tests memory behaviour. Is separated from rest because on the future 
+//it might spend more than one cycle to coplete each instruction
+
+localparam PROGRAM_MEMORY_SIZE=64;
 
 reg clk;
 reg reset;
@@ -21,20 +25,20 @@ integer i;
 
 initial
 begin
-    $dumpfile("assembly_instructions.vcd");
+    $info("Testing memory behaviour from assembly file. x31 is probed for pass, must always be zero!!!");
+    $dumpfile(`VCDFILEPATH);
     $dumpvars(0,mut);
-    //$monitor("%2t,reset=%b,clk=%b,pc=%d,current instruction=%h, x31=%d",$time,reset,clk,mut.pc,mut.instruction,mut.single_instr.reg_mem.memory[31]);
+    //$monitor("%2t,reset=%b,clk=%b,pc=%d,current instruction=%h, x1=%h, x10=%h,x11=%h, x31=%h",$time,reset,clk,mut.pc,mut.instruction,mut.single_instr.reg_mem.memory[1],mut.single_instr.reg_mem.memory[10],mut.single_instr.reg_mem.memory[11],mut.single_instr.reg_mem.memory[31]);
 
-    $info("Testing from assembly file. x31 is probed for pass, must always be zero!!!s");
-    $readmemh("arquivo.mem", mut.program_memory,0,31);
+    $readmemh(`MEMFILEPATH, mut.program_memory,0,PROGRAM_MEMORY_SIZE-1);
 
     reset=1;#20;
 
     @(negedge clk) #1; reset=0;
     
-    for (i=0; i < 32;i=i+1) begin
+    for (i=0; i < PROGRAM_MEMORY_SIZE;i=i+1) begin
 	    $display("PC is %d, instruction: %h", mut.pc, mut.instruction);
-	    @(posedge clk) #1; `assertCaseEqual(mut.single_instr.reg_mem.memory[31],32'd 0, "x31 register has to be zero");
+	    @(posedge clk) #1; `assertCaseEqual(mut.single_instr.reg_mem.memory[31],32'd 0, {"x31 register has to be zero"});
     end
     #1;
     $display("Simulation finished");
