c79678da4fa16754e1d53cc24b6935c9a407902d
Gustavo Nunes Martins <martins.yo@gmail.com>
2021-11-26 08:20:41 +0100

    Tests: Easier to understand by making instruction under test in CAPITAL LETTERS

--- tests/03_assembly/test_addi.s
+++ tests/03_assembly/test_addi.s
@@ -2,22 +2,22 @@ addi x31,x0,0           // STARTUP: Any test should pass if and only if x31 is z
 addi x1,x0,1            // Sub-test number
 ADDI x21, x0, 123       // BEGIN:   Test ADDI start, setting expected value
 ADDI x20, x0, 123
-SUB x31, x20, x21       // END:     Control variable: X31 is zero if and only if circuit is correct. Finish test
+sub x31, x20, x21       // END:     Control variable: X31 is zero if and only if circuit is correct. Finish test
 
 addi x1,x1,1            // Sub-test number
 addi x10,x0,1110        // BEGIN:   Test ADD start, setting expected value
 addi x5,x0,555
 addi x2,x0,555
-add x3,x5,x2
+ADD x3,x5,x2
 sub x31,x3,x10          // END:     Control variable should be zero if passed
 
 addi x1,x1,1            // Sub-test number
-lui x5, 1               // BEGIN:   Tests LUI. 4096 is lui 1, because its the 13th bit (2^12)
+LUI x5, 1               // BEGIN:   Tests LUI. 4096 is lui 1, because its the 13th bit (2^12)
 addi x2,x0,2047         //          4096 = 2047+2047+2
 addi x2,x2,2047
 addi x2,x2,2
 sub x31,x5,x2           // END:     Control variable: X31 has to be zero when 4096 - 4096 = 0
 
 addi x1,x1,1            // Sub-test number
-addi x5,x0,2047         // BEGIN:   Test negative numbers on ADDI
-addi x31,x5,-2047       // END:     Control variable: X31 should be zero
+ADDI x5,x0,2047         // BEGIN:   Test negative numbers on ADDI
+ADDI x31,x5,-2047       // END:     Control variable: X31 should be zero
--- tests/03_assembly/test_sw_lw.s
+++ tests/03_assembly/test_sw_lw.s
@@ -3,8 +3,8 @@ addi x31,x0,0           // STARTUP: Any test should pass if and only if x31 is z
 addi x1,x0,1            // First test
 addi x5, x0, -500       // BEGIN:   STORE WORD, followed by LOAD WORD.
 addi x2, x0, 1024       //          Will write to memory position 1024
-sw x5, 0(x2)            //          Copies contents of x5 into memory given by x2
-lw x3,0(x2)             //          Reads from mem pos 1024 (x2) into x3
+SW x5, 0(x2)            //          Copies contents of x5 into memory given by x2
+LW x3,0(x2)             //          Reads from mem pos 1024 (x2) into x3
 sub x31,x5,x3           // END:     Control variable: Register 31 should be zero
 
 addi x1,x1,1            // Second test
@@ -12,31 +12,31 @@ addi x5,x0,-653         // BEGIN:   STORE WORD with negative offset, followed by
 lui      x2,1           //          Loads 4548
 addi    x2,x2,452       //          Loads 4548
 addi x3,x0,453          //          Will read from memory position 2500=2047+453 (has to be different offset than before, otherwise test is worthless)
-sw x5, -2048(x2)        //          Copies content of x5 into memory address 2500=4548-2048
-lw x4, 2047(x3)         //          Loads cotentents of 2500=453+2047 into x3
+SW x5, -2048(x2)        //          Copies content of x5 into memory address 2500=4548-2048
+LW x4, 2047(x3)         //          Loads cotentents of 2500=453+2047 into x3
 sub x31,x5,x4           // END:     Control variable: Register 31 should be zero
 
 addi x1,x1,1            // Third test
 lui      x2,1           // BEGIN:   Loads 4548
 addi    x2,x2,452       //          Loads 4548
 addi x3,x0,453          //          Will read from memory position 2500=2047+453 (has to be different offset than before, otherwise test is worthless)
-sw x5, 2047(x3)         //          Copies content of x5 into memory address 2500=453+2047
-lw x4, -2048(x2)        //          Loads cotentents of 2500=4548-2048 into x3
+SW x5, 2047(x3)         //          Copies content of x5 into memory address 2500=453+2047
+LW x4, -2048(x2)        //          Loads cotentents of 2500=4548-2048 into x3
 sub x31,x5,x4           // END:     Control variable: Register 31 should be zero
 
 addi x1,x1,1            // Fourth test
 addi x5,x0,100          // BEGIN:
 addi x2,x0,200
 addi x3,x0,400
-sw x5, 51(x5)           //          Saves 100 into 151 (51 uses both part of immediate)
-sw x2, 2047(x2)         //          Saves 200 into 2247
-sw x3, -331(x3)         //          Saves 400 into 69. Assigning multiple times wasnt covered on previous tests
+SW x5, 51(x5)           //          Saves 100 into 151 (51 uses both part of immediate)
+SW x2, 2047(x2)         //          Saves 200 into 2247
+SW x3, -331(x3)         //          Saves 400 into 69. Assigning multiple times wasnt covered on previous tests
 addi x11,x0,148         //          Will read from 151(148+3), 2247(200+2047), 69(400-331). This is done to test offsetting
 addi x12,x0,200
 addi x13,x0,84
-lw x11, 3(x11)
-lw x12, 2047(x12)
-lw x13, -15(x13)
+LW x11, 3(x11)
+LW x12, 2047(x12)
+LW x13, -15(x13)
 sub x31,x5,x11           
 sub x31,x2,x12           
 sub x31,x3,x13          // END:
