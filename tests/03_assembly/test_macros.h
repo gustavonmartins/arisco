f8d19b3a07a8ab1884ae7664184ae26a5fdf65b7
Gustavo Nunes Martins <martins.yo@gmail.com>
2021-12-09 22:24:39 +0100

    Test macro: small formatting

--- tests/03_assembly/test_macros.h
+++ tests/03_assembly/test_macros.h
@@ -8,6 +8,6 @@
     li x3, reg_1_content;                                                           \
     li x4, reg_2_content;                                                           \
     instruction x5, x3, x4;                                                         \
-    sub x31, x5, x2 // Verilog testbench is configured to detect x31=0 as a sign of passing. For debug, to get the generated value, set expected to zero
-
+    sub x31, x5, x2 // Verilog testbench is configured to detect x31=0 as a sign of passing.
+                    // For debug, to get the generated value, set expected to zero
 #endif
