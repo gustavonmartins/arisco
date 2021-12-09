b0aadf2d6dccfe67a6cafb4458dacccc1a9e52a1
Gustavo Nunes Martins <martins.yo@gmail.com>
2021-12-09 22:13:33 +0100

    SRA works

--- tests/03_assembly/test_macros.h
+++ tests/03_assembly/test_macros.h
@@ -1,13 +1,13 @@
 #ifndef __TEST_MACROS
 #define __TEST_MACROS
 
-#define TEST_R(test_id, instruction, val1, val2, expected_result) \
-    addi x31, x0, 0;                                              \
-    li x1, test_id;                                               \
-    li x2, expected_result;                                       \
-    li x3, val1;                                                  \
-    li x4, val2;                                                  \
-    instruction x5, x3, x4;                                       \
-    sub x31, x2, x5 // Verilog testbench is configured to detect x31=0 as a sign of passing.
+#define TEST_R(test_id, instruction, reg_1_content, reg_2_content, expected_result) \
+    addi x31, x0, 0;                                                                \
+    li x1, test_id;                                                                 \
+    li x2, expected_result;                                                         \
+    li x3, reg_1_content;                                                           \
+    li x4, reg_2_content;                                                           \
+    instruction x5, x3, x4;                                                         \
+    sub x31, x5, x2 // Verilog testbench is configured to detect x31=0 as a sign of passing. For debug, to get the generated value, set expected to zero
 
 #endif
