09330a60b0e39f4a5bb008a61b8bc3a69d5f5c47
Gustavo Nunes Martins <martins.yo@gmail.com>
2021-12-19 15:04:36 +0100

    VGA: Test moved to always block to be safer

--- rtl/vga/tests/VGA_tb.v
+++ rtl/vga/tests/VGA_tb.v
@@ -2,82 +2,60 @@
 `include "VGA.v"
 module VGA_tb ();
 
-VGA mut(.clk25175KHz(clk), .reset(reset));
+    VGA mut(.clk25175KHz(clk), .reset(reset));
 
-reg clk;
-reg reset;
+    reg clk;
+    reg reset;
 
-initial begin
-    clk=0;
-    
-end
-always begin
-    #5;clk=~clk;
-end
+    reg [31:0] i;
+    reg [9:0] test_hcount;
+    reg [9:0] test_vcount;
 
-always @(posedge clk) begin
-    //always@(posedge clk) hcount <= (reset==1 || hcount!=799) ? 0 : hcount+1; //This would be better than on initial
-
-    if (test_hcount >= 0 && test_hcount < 96 &&  mut.hSync !== 0 ) begin
-            $display("%c[1;31m",27);
-            $display("Hsync should be low for 96 cycles after reset is released");
-            $display("%c[0m",27);
-            $fatal();
-    end else if (test_hcount>=96 && mut.hSync !== 1) begin
-        $display("%c[1;31m",27);
-            $display("Hsync should be high  most of the times");
-            $display("%c[0m",27);
-            $fatal();
+    always begin
+        #5;clk=~clk;
     end
 
-    if (test_vcount >= 0 && test_vcount < 2 &&  mut.vSync !== 0 ) begin
-            $display("%c[1;31m",27);
-            $display("Vsync should be low for 2 cycles after reset is released");
-            $display("%c[0m",27);
-            $fatal();
-    end else if (test_vcount>=2 && mut.vSync !== 1) begin
-        $display("%c[1;31m",27);
-            $display("Vsync should be high  most of the times");
-            $display("%c[0m",27);
-            $fatal();
-    end
-end
-
-integer i;
-integer test_hcount, test_vcount;
-
-initial begin
-    $dumpfile(`VCDFILEPATH);
-    $dumpvars(0,mut,test_hcount,test_vcount);
-
-    reset=1;
-    @(posedge clk);@(posedge clk);@(posedge clk);@(posedge clk);@(posedge clk);#1;
-    reset=0;
-
-    @(posedge clk);
-    i=0;
-    test_hcount=0;test_vcount=0;
-    
-    while (i<800*524*2) begin 
-        @(posedge clk)
-        i+=1;
-        if (test_hcount == 799 && test_vcount != 524) begin
-            test_hcount=0;
-            test_vcount+=1;
+    always @(posedge clk) begin
+        //always@(posedge clk) hcount <= (reset==1 || hcount!=799) ? 0 : hcount+1; //This would be better than on initial
+        if (reset===1) begin 
+            test_hcount<=0;
+            test_vcount<=0;
+        end else if (test_hcount == 799 && test_vcount != 524) begin
+            test_hcount<=0;
+            test_vcount<=test_vcount+1;
         end else if (test_hcount != 799 && test_vcount != 524) begin 
-            test_hcount+=1;
+            test_hcount<=test_hcount+1;
         end else if (test_hcount != 799 && test_vcount == 524) begin 
-            test_hcount+=1;
+            test_hcount<=test_hcount+1;
         end else if (test_hcount == 799 && test_vcount == 524) begin 
-            test_vcount=0;
-            test_hcount=0;
+            test_vcount<=0;
+            test_hcount<=0;
         end 
+
+        i<=i+1;
+
+        if (i>800*524*2) begin 
+            $display("Simulation finished");
+            $finish;
+        end
     end
 
- 
+    
 
-    $display("Simulation finished");
-    $finish;
-end
+    initial begin
+        $dumpfile(`VCDFILEPATH);
+        $dumpvars(0,mut,test_hcount,test_vcount);
+
+        clk=0;
+
+        i=0;
+        test_hcount=0;test_vcount=0;
+
+        reset=1;
+        @(posedge clk);@(posedge clk);@(posedge clk);@(posedge clk);@(posedge clk);
+        #1;
+        reset=0;
+
+    end
 
 endmodule
