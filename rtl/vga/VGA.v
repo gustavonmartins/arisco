07d0514fb0b7755951ed299cb0e80f6b46445f29
Gustavo Nunes Martins <martins.yo@gmail.com>
2022-01-01 22:59:09 +0100

    VGA: Can be synthethized

--- rtl/vga/VGA.v
+++ rtl/vga/VGA.v
@@ -1,33 +1,33 @@
 `default_nettype none
 
 
-module VGA(input clk25175KHz, input reset, 
-    output [9:0] x, output [8:0] y,
-    input redIn, input greenIn, input blueIn, 
-    output redOut,output greenOut, output blueOut,
-    output reg hSync, output reg vSync, output reg [9:0] hcount,  output reg [9:0] vcount);
+module VGA(input clk25175KHz, 
+   
+    output wire hSync, output wire vSync);
+
+    reg [9:0] hcount;
+    reg [18:0] framecount;
+
+    localparam PIXELS_PER_LINE=800;
+    localparam LINES_PER_FRAME=525;
+    localparam PIXELS_PER_FRAME = LINES_PER_FRAME*PIXELS_PER_LINE;
 
     //int hcount;
     initial begin
         hcount<=0;
-        vcount<=0;
+        framecount<=0;
     end
 
     always @(posedge clk25175KHz) begin
-        casez (reset)
-            1'b 1 : begin 
-                    hSync<=0; hcount<=0;
-                    vSync<=0; vcount<=0; 
-                end
-            1'b 0 : begin 
+        
                     // Updates counts and only then gets sync. If you check sync before counter, you get wrong results!
-                    hcount=(hcount == 799)? 0 :hcount+1;
-                    vcount=(hcount == 0)? ((vcount === 524)? 0 :vcount+1) : vcount;
+                    hcount<=(hcount == PIXELS_PER_LINE-1)? 0 :hcount+1;
+                    framecount<=(framecount == PIXELS_PER_FRAME-1)?0:framecount+1;
 
                     // Selects sync. Do not put this before updating counter!
-                    hSync<=(hcount < 96)? 0: 1;
-                    vSync<=(vcount < 2)? 0: 1;
-                end
-        endcase
+                   
+                
     end
+    assign hSync=(hcount > PIXELS_PER_LINE -96)? 0: 1;
+    assign vSync=(framecount > PIXELS_PER_FRAME-PIXELS_PER_LINE*2)? 0: 1;
 endmodule
--- rtl/vga/tests/VGA_tb.v
+++ rtl/vga/tests/VGA_tb.v
@@ -2,14 +2,13 @@
 `include "VGA.v"
 module VGA_tb ();
 
-    VGA mut(.clk25175KHz(clk), .reset(reset));
+    VGA mut(.clk25175KHz(clk));
 
     reg clk;
-    reg reset;
 
     reg [31:0] i;
     reg [9:0] test_hcount;
-    reg [9:0] test_vcount;
+    reg [18:0] test_framecount;
 
     always begin
         #5;clk=~clk;
@@ -19,70 +18,54 @@ module VGA_tb ();
 
         //Updates counters
         //always@(posedge clk) hcount <= (reset==1 || hcount!=799) ? 0 : hcount+1; //This would be better than on initial
-        if (reset===1) begin 
+        if (test_hcount == 800-1)
             test_hcount<=0;
-            test_vcount<=0;
-        end else if (test_hcount == 799 && test_vcount != 524) begin
-            test_hcount<=0;
-            test_vcount<=test_vcount+1;
-        end else if (test_hcount != 799 && test_vcount != 524) begin 
+        if (test_hcount < 800-1)
             test_hcount<=test_hcount+1;
-        end else if (test_hcount != 799 && test_vcount == 524) begin 
-            test_hcount<=test_hcount+1;
-        end else if (test_hcount == 799 && test_vcount == 524) begin 
-            test_vcount<=0;
-            test_hcount<=0;
-        end 
 
-        // Specifies how signals should be
-        if (reset !== 1 && test_hcount >= 0 && test_hcount < 96 &&  mut.hSync !== 0 ) begin
-            $display("%c[1;31m",27);
-            $display("Hsync should be low for 96 cycles after reset is released");
-            $display("%c[0m",27);
-            $fatal();
-        end else if (reset !== 1 && test_hcount>=96 && mut.hSync !== 1) begin
-            $display("%c[1;31m",27);
-            $display("Hsync should be high beggining at 96 pixes");
-            $display("%c[0m",27);
-            $fatal();
+        if (test_framecount == 800*525-1)
+            test_framecount<=0;
+        if (test_framecount < 800*525-1) 
+            test_framecount <= test_framecount+1;
+        else begin
+            test_framecount <=0;
+            test_hcount <= 0;
         end
-
-        if (reset !== 1 && test_vcount >= 0 && test_vcount < 2 &&  mut.vSync !== 0 ) begin
-                $display("%c[1;31m",27);
-                $display("Vsync should be low for 2 cycles after reset is released");
-                $display("%c[0m",27);
-                $fatal();
-        end else if (reset !== 1 && test_vcount>=2 && mut.vSync !== 1) begin
-            $display("%c[1;31m",27);
-                $display("Vsync should be high  most of the times");
-                $display("%c[0m",27);
-                $fatal();
-        end
-
         i<=i+1;
 
-        if (i>800*524*2) begin 
+        if (i>800*525*2) begin 
             $display("Simulation finished");
             $finish;
         end
+    end
+
+    always @(posedge clk) begin
+         // Specifies how signals should be
+        if (test_hcount > 800-96 &&  mut.hSync !== 0 ) begin
+            $display("%c[1;31m",27);
+            $display("Hsync should be low for 96 lines");
+            $display("%c[0m",27);
+            $fatal();
+        end
+
+        if (test_framecount > 800*525-800*2 &&  mut.vSync !== 0 ) begin
+                $display("%c[1;31m",27);
+                $display("Vsync should be low for 2 lines ");
+                $display("%c[0m",27);
+                $fatal();
+        end 
     end
 
     
 
     initial begin
         $dumpfile(`VCDFILEPATH);
-        $dumpvars(0,mut,test_hcount,test_vcount);
+        $dumpvars(0,clk,mut,test_hcount,test_framecount);
 
         clk=0;
 
-        i=0;
-        test_hcount=0;test_vcount=0;
-
-        reset=1;
-        @(posedge clk);@(posedge clk);@(posedge clk);@(posedge clk);@(posedge clk);
-        #1;
-        reset=0;
-
+        i<=0;
+        test_hcount<=0;test_framecount<=0;
     end
 
 endmodule
