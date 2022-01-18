0f4292fb71bfe527d191009ec114f7e2bb59fde5
Gustavo Nunes Martins <martins.yo@gmail.com>
2022-01-18 21:39:38 +0100

    Synth: Start moving ROM in separate module to prepare synthetizable ROM

--- rtl/ROM.v
+++ rtl/ROM.v
@@ -2,24 +2,26 @@
 `include "rtl/parameters.vh"
 
 module ROM 
-#(parameter PROGRAM_MEMORY_SIZE_WORDS = 6)
+//#(parameter PROGRAM_MEMORY_SIZE_WORDS = 6)
 (input wire clk, 
-    input wire [31:0] address,
-    output wire [31:0] read_data
+    input wire [31:0] rom_address,
+    output reg [31:0] inst_buffer
 );
     
-    reg [32-1:0] internal [0:PROGRAM_MEMORY_SIZE_WORDS-1];
+    reg [32-1:0] program_memory[PROGRAM_MEMORY_SIZE_WORDS-1:0];
 
-    initial begin
-        internal[0]<=32'h 00000000;
-    end
+    `ifdef SYNTH
+        initial begin
+            program_memory[0]<=32'h 00000000;
+        end
+    `endif
 
-    reg [32-1:0] outbuffer;
+    //reg [32-1:0] inst_buffer;
 
     always @(negedge clk) begin
-        outbuffer <= internal[address>>2];
+        inst_buffer <= program_memory[rom_address>>2];
     end
 
-    assign read_data=outbuffer;
+    //assign read_data=inst_buffer;
 
 endmodule
--- rtl/SoC.v
+++ rtl/SoC.v
@@ -12,29 +12,15 @@ module SoC (
         .o_bus_address(bus_address),.o_bus_wr_data(bus_wr_data),.o_bus_write_length(bus_write_length),.o_bus_wr_enable(bus_wr_enable),
         .i_bus_read_data(bus_read_data),
         .o_bus_rom_pc(rom_address),
-        .i_instruction(instruction));
+        .i_instruction(rom_read_data));
 
 
     wire [31:0] rom_address,rom_read_data;
 
 
-
-    
-    reg [31:0] program_memory[PROGRAM_MEMORY_SIZE_WORDS-1:0];
-    wire [31:0] instruction=inst_buffer;
-    reg [31:0] inst_buffer;
-
-    always @(negedge clk) begin
-        inst_buffer <= program_memory[rom_address>>2];
-    end
-
-    `ifdef SYNTH
-    initial begin
-        program_memory[0] <= 32'h 00000000;
-    end
-    `endif
-
     assign dummy=rom_address[7:0];
+
+    ROM rom(.clk(clk), .rom_address(rom_address), .inst_buffer(rom_read_data));
 
     // Main memory
     RAM mainMemory (.clk (clk), .address (bus_address), 
