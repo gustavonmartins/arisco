85ab9495a266d3290871b7eaee3b0205cc980fd2
Gustavo Nunes Martins <martins.yo@gmail.com>
2022-09-30 22:10:27 +0200

    Test macros has higher common components

--- tests/03_assembly/test_macros.h
+++ tests/03_assembly/test_macros.h
@@ -49,19 +49,19 @@ i7: if reaches this instead of jumping back, fail.
 #define TEST_R(test_id, instruction, reg_1_content, reg_2_content, expected_result) \
     li x31, 0;                                                                      \
     li x1, test_id;                                                                 \
-    li x2, reg_1_content;                                                           \
-    li x3, reg_2_content;                                                           \
-    instruction x29, x2, x3;                                                        \
-    li x30, expected_result;                                                        \
-    sub x31, x29, x30;
+    li x2, expected_result;                                                         \
+    li x3, reg_1_content;                                                           \
+    li x4, reg_2_content;                                                           \
+    instruction x29, x3, x4;                                                        \
+    sub x31, x29, x2;
 
 #define TEST_I(test_id, instruction, reg_1_content, immediate_content, expected_result) \
     li x31, 0;                                                                          \
     li x1, test_id;                                                                     \
-    li x2, reg_1_content;                                                               \
-    li x3, expected_result;                                                             \
-    instruction x29, x2, immediate_content;                                             \
-    sub x31, x29, x3;
+    li x2, expected_result;                                                             \
+    li x3, reg_1_content;                                                               \
+    instruction x29, x3, immediate_content;                                             \
+    sub x31, x29, x2;
 
 #define TEST_SW_LW(test_id, content, store_base, store_offset, load_base, load_offset) \
     li x31, 0;                                                                         \
