7afda2d5cef686c176165db20cab022afe73c3f6
Gustavo Nunes Martins <martins.yo@gmail.com>
2022-09-30 20:20:27 +0200

    Preparing to make SW/LW tests easier

--- tests/03_assembly/test_macros.h
+++ tests/03_assembly/test_macros.h
@@ -31,6 +31,16 @@
     li x30, expected_result;                                                            \
     li x31, test_id;
 
+#define TEST_SW_LW(test_id, content, store_base, store_offset, load_base, load_offset) \
+    li x31, 0;                                                                         \
+    li x1, test_id;                                                                    \
+    li x2, content;                                                                    \
+    li x3, store_base;                                                                 \
+    li x4, load_base;                                                                  \
+    SW x2, store_offset(x3);                                                           \
+    LW x29, load_offset(x4);                                                           \
+    li x30, content;                                                                   \
+    li x31, test_id;
 /***
 i1
 i2 should jump to m1 now
